3678b5c8064160971fba3bbab2731dc09dc395d3
unknown <baguseka0898@gmail.com>
2021-03-01 11:14:16 +0700

    bruh commit

--- Screen/History/History.js
+++ Screen/History/History.js
@@ -1,67 +1,13 @@
-import React, { Component, useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
-import storage from '@react-native-firebase/storage';
-import firestore from '@react-native-firebase/firestore';
-import styles from './style';
-import { DataTable } from 'react-native-paper';
+import React, { Component } from 'react'
+import { Alert, FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 
-const History = ({ navigation, route }) => {
-    const { dataID } = route.params;
-    const [name, setName] = useState(dataID)
-    const [jamMasuk, setUser] = useState([]);
-
-    useEffect(() => {
-        const dataa = firestore()
-            .collection('Checkin')
-            .where('email', '==', name)
-            .onSnapshot(querySnapshot => {
-                // console.log('Total users: ', querySnapshot.size);
-                const jamMasuk = [];
-                querySnapshot.forEach(documentSnapshot => {
-                    jamMasuk.push({
-                        ...documentSnapshot.data(),
-                        key: documentSnapshot.id,
-                    });
-
-                });
-                setUser(jamMasuk);
-            });
-        return () => dataa();
-    }, [])
-
-    const renderItem = ({ item, index }) => {
+export default class History extends Component {
+    render() {
         return (
-            <View>
-                <View style={{ backgroundColor: 'powderblue', flexDirection: 'row' }}>
-                    <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>{item.hari}</Text></View>
-                    <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>{item.jamMasuk}</Text></View>
-                    <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>{item.jamMasuk}</Text></View>
-                    <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>-</Text></View>
-                </View>
-
-            </View>
+           <Text>
+               test
+           </Text>
 
         )
     }
-
-
-    return (
-        <View >
-            <View style={{ backgroundColor: 'powderblue', flexDirection: 'row' }}>
-                <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderRadius: 1 }}><Text style={{ fontSize: 20 }}>Tgl</Text></View>
-                <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>CheckIn</Text></View>
-                <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>CheckOut</Text></View>
-                <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>lembur</Text></View>
-            </View>
-            <View style={{ paddingTop: 30 }}>
-                <FlatList
-                    data={jamMasuk}
-                    renderItem={renderItem}
-                />
-            </View>
-        </View >
-    );
-
 }
-
-export default History;
